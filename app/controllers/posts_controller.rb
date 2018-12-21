782c52a177e028967f54d939b222b7174f24a5e7
Jeriael S. Diaz <jeriaelsdiaz@gmail.com>
2018-12-21 00:21:59 -0500

    added CRUD and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-      def index
-        @posts = Post.all
+        def index
+      if params[:category].blank?
+        @posts = Post.all.order("created_at DESC")
+        else
+        @category_id = Category.find_by(name: params[:category]).id
+        @posts = Post.where(category_id: @category_id).order("created_at DESC")
+        end
       end
 
       def show
@@ -34,9 +39,11 @@ class PostsController < ApplicationController
     end
 
       def destroy
+          @post.destroy
+          redirect_to root_path, notice: "Deletion Completion"
       end
 
-private
+    private
 
       def post_params
         params.require(:post) .permit(:title, :content, :category_id)
